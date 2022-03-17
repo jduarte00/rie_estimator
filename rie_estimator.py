c538ef2335409472de10db0296d268ad196c8f21
Jose Duarte <joseduarte@192.168.1.11>
2022-03-17 08:57:58 -0600

    delete debug prints

--- rie_estimator.py
+++ rie_estimator.py
@@ -3,29 +3,28 @@ import pandas as pd
 from numpy import linalg as LA
 
 __author__ = 'José Antonio Duarte'
-#__copyright__ = 'Copyright 2017-2022 Gregory Giecold and contributors'
 __credit__ = 'José Antonio Duarte'
 __status__ = 'beta'
 __version__ = '0.1.0'
-
-# https://stackoverflow.com/questions/44834/can-someone-explain-all-in-python
-__all__ = ['RIE_estimator', 'returnsStandardization']
+__all__ = ['get_rie', 'returnsStandardization']
 
 
 def returnsStandardization(returns):
-    returns_wth_mean = returns - np.mean(returns, axis = 0)
-    hat_sigma = np.sqrt((returns_wth_mean**2).sum(axis = 1))
-    r_tilde = returns_wth_mean.divide(hat_sigma, axis =0)
+    returns_wth_mean = returns - np.mean(returns, axis=0)
+    hat_sigma = np.sqrt((returns_wth_mean**2).sum(axis=1))
+    r_tilde = returns_wth_mean.divide(hat_sigma, axis=0)
     X = r_tilde / np.std(r_tilde)
     return X
 
 # The returns should be a Dataframe of size T X N without NULL values
-def RIE_estimator(returns, normalize = False):
+
+
+def get_rie(returns, normalize=False):
     def get_s_k(index_lambda, N):
-        return 1/N* (sum(1/(z_k[index_lambda]- lambdas)) - 1/(z_k[index_lambda] - lambdas[index_lambda]))
-    
-    # T is the number of observations, N is the number of assets 
-    T,N = returns.shape
+        return 1/N * (sum(1/(z_k[index_lambda] - lambdas)) - 1/(z_k[index_lambda] - lambdas[index_lambda]))
+
+    # T is the number of observations, N is the number of assets
+    T, N = returns.shape
     RIE_estimator = np.zeros((N, N), dtype=float)
     if normalize:
         returns = returnsStandardization(returns)
@@ -36,23 +35,25 @@ def RIE_estimator(returns, normalize = False):
     u_ks = u_ks.T
     n_lambda = lambdas[0]
     q = float(N/T)
-    sigma_sq = (n_lambda)/(1- np.sqrt(q))**2
+    sigma_sq = (n_lambda)/(1 - np.sqrt(q))**2
     lambda_plus = n_lambda*((1+np.sqrt(q))/(1 - np.sqrt(q)))**2
     # Get z_k
     z_k = lambdas - (1j / np.sqrt(N))
-    print(z_k)
     # Get s_k(z_k)
-    s_k = list(map(lambda index_lambda: get_s_k(index_lambda,N), np.argsort(lambdas)))
+    s_k = list(map(lambda index_lambda: get_s_k(
+        index_lambda, N), np.argsort(lambdas)))
     # Get \xi_k^{RIE}
-    xi_k = lambdas / np.abs(1 - q + q *z_k * s_k)**2
-    #Get stieltjes g_{mp}(z)
-    g_mp = (z_k + sigma_sq*(q-1) - (np.sqrt(z_k - n_lambda)* np.sqrt(z_k - lambda_plus)))/(2*q*z_k*sigma_sq)
+    xi_k = lambdas / np.abs(1 - q + q * z_k * s_k)**2
+    # Get stieltjes g_{mp}(z)
+    g_mp = (z_k + sigma_sq*(q-1) - (np.sqrt(z_k - n_lambda)
+            * np.sqrt(z_k - lambda_plus)))/(2*q*z_k*sigma_sq)
     # Get gamma_k(z_k)
     gamma_k = sigma_sq * ((np.abs(1 - q + q*z_k*g_mp)**2)/(lambdas))
     # Get \hat{xh}_k
-    xi_hat = list(map(lambda xi, gamma: xi * gamma if gamma > 1 else xi, xi_k, gamma_k))
+    xi_hat = list(map(lambda xi, gamma: xi *
+                  gamma if gamma > 1 else xi, xi_k, gamma_k))
     # Get RIE
     for xi, u_i in zip(xi_hat, u_ks):
         RIE_estimator += xi*(u_i.reshape(-1, 1) @ u_i.reshape(-1, 1).T)
-        
+
     return RIE_estimator
