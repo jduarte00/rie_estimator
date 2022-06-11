e12f8e58ec7a5384e5934f52a2e746c95ecaaea9
Jose Duarte <dumejo92@gmail.com>
2022-06-11 18:53:15 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rie_estimator",
-    version="v0.0.3-beta",
+    version="v0.0.4-beta",
     author="José Antonio Duarte Mendieta",
     author_email="jose.duarte@cimat.mx",
     description="Function to estimate the oracle RIE corrrelation estimator of a dataset",
